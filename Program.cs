4ca1292657b90720f5615011477066d5d9d35167
ingel81 <sworddeamon@gmx.de>
2021-11-22 16:50:12 +0100

    Cleanup

--- Program.cs
+++ Program.cs
@@ -7,47 +7,78 @@ namespace IotNameGenerator
 {
   class Program
   {
+    const string SEPERATOR = " ";
+    const string JOINSEPERATOR = ", ";
+
     static void Main(string[] args)
     {
-      Console.WriteLine("Bitte kommagetrennte Liste der Geräteart eingeben  (Was wird geschaltet, z.b.: Licht,Lampe,Beleuchtung,Spots)");
+      Console.WriteLine(@$"
+         _____              ______                    
+        (_____)     _      |  ___ \                   
+           _   ___ | |_    | |   | | ____ ____   ____ 
+          | | / _ \|  _)   | |   | |/ _  |    \ / _  )
+         _| || |_| | |__   | |   | ( ( | | | | ( (/ / 
+        (_____)___/ \___)  |_|   |_|\_||_|_|_|_|\____)
+
+          ______                                              
+         / _____)                             _               
+        | /  ___  ____ ____   ____  ____ ____| |_  ___   ____ 
+        | | (___)/ _  )  _ \ / _  )/ ___) _  |  _)/ _ \ / ___)
+        | \____/( (/ /| | | ( (/ /| |  ( ( | | |_| |_| | |    
+         \_____/ \____)_| |_|\____)_|   \_||_|\___)___/|_|
+                                                              v0.1
+        ");
+
+      PrintOuput(BuildVariants(Input()));
+    }
+
+    private static (string[] Groups, string[] Places, string[] Floors) Input()
+    {
+      Console.WriteLine("\nKommagetrennte Liste der Geräteart (Was wird geschaltet, z.B.: Licht,Lampe,Beleuchtung,Spots)");
+      Console.Write(">> ");
       var groupInput = Console.ReadLine();
 
-      Console.WriteLine("Bitte kommagetrennte Liste der Bezeichner eingeben (Wo befindet es sich, z.B.: Esstisch,Esszimmer)");
+      Console.WriteLine("\nKommagetrennte Liste der Bezeichner eingeben (Wo befindet es sich, z.B.: Esstisch,Esszimmer)");
+      Console.Write(">> ");
       var placesInput = Console.ReadLine();
 
-      Console.WriteLine("zusätzlichen kommagetrennte Etagenbezeichner eingeben (Etage, z.B.: EG,Erdgeschoss)");
+      Console.Write(">> ");
+      Console.WriteLine("\n(Optional) Kommagetrennten Etagenbezeichner eingeben (Etage, z.B.: EG,Erdgeschoss)");
       var floorInput = Console.ReadLine();
-
-      Console.WriteLine("Auch Varianten ohne Etagenbezeichner generieren? (true/false - default true)");
-      if (bool.TryParse(Console.ReadLine(), out var doEmptyFloor) == false)
-        doEmptyFloor = true;
 
       var groups = groupInput.Split(",", StringSplitOptions.RemoveEmptyEntries);
       var places = placesInput.Split(",", StringSplitOptions.RemoveEmptyEntries);
       var floors = new List<string>();
 
-      if (doEmptyFloor)
-        floors.Add(string.Empty);
-
       if (floorInput != string.Empty)
+      {
         floors.AddRange(floorInput.Split(",", StringSplitOptions.RemoveEmptyEntries).ToList());
 
+        Console.WriteLine("\nAuch Varianten ohne Etagenbezeichner generieren? (\"true\"/\"false\" - default: \"true\" = Enter)");
+        Console.Write(">> ");
+        if (bool.TryParse(Console.ReadLine(), out var doEmptyFloor) == false)
+          doEmptyFloor = true;
 
-      var variants = new List<string>();
-      var seperator = " ";
+        if (doEmptyFloor)
+          floors.Add(string.Empty);
+      }
 
-      foreach (var floor in floors)
-        foreach (var place in places)
-          foreach (var group in groups)
-            AddMixed(variants, group, place, floor, seperator);
-
-      var deDupedVariants = variants.Distinct().ToList();
-      Console.WriteLine($"Variants: {deDupedVariants.Count} deduped");
-      Console.WriteLine();
-      Console.WriteLine(string.Join(", ", deDupedVariants.ToArray()));
+      return (groups, places, floors.ToArray());
     }
 
-    static void AddMixed(List<string> variants, string group, string place, string floor, string seperator)
+    private static List<string> BuildVariants((string[] Groups, string[] Places, string[] Floors) Input)
+    {
+      var result = new List<string>();
+
+      foreach (var floor in Input.Floors)
+        foreach (var place in Input.Places)
+          foreach (var group in Input.Groups)
+            AddMixed(result, group, place, floor, SEPERATOR);
+
+      return result.Distinct().ToList();
+    }
+
+    private static void AddMixed(List<string> variants, string group, string place, string floor, string seperator)
     {
       if (floor == string.Empty)
       {
@@ -63,6 +94,16 @@ namespace IotNameGenerator
         variants.Add($"{group}{seperator}{floor}{seperator}{place}");
         variants.Add($"{group}{seperator}{place}{seperator}{floor}");
       }
+    }
+
+    private static void PrintOuput(List<string> output)
+    {
+      Console.WriteLine($"\n#########################");
+      Console.WriteLine($"Varianten: {output.Count}\n");
+      Console.WriteLine(string.Join(JOINSEPERATOR, output.ToArray()));
+      Console.WriteLine("\n\nBitte beliebige Taste drücken um zu beenden.");
+      Console.Write(">> ");
+      Console.ReadKey();
     }
 
   }
